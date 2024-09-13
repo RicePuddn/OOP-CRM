69bcf2e56f7a75cb550957fa089e8d2b4df6100f
RicePuddn <ernesthengmengwee@gmail.com>
2024-09-13 21:11:31 +0800

    Added Routing

--- olive-crm/src/App.tsx
+++ olive-crm/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./component/ui/button";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./component/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div>
+          <nav>
+            <ul className="flex space-x-4 bg-gray-100 p-4">
+              <li>
+                <Link to="/" className="text-blue-500 hover:text-blue-700">
+                  Home
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
