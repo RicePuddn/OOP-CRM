9e999062a49b7ade4d43e29dd306a2404734e6a4
Yi Ywe Lin <73067851+yiywelin@users.noreply.github.com>
2024-10-16 00:38:39 +0800

    Exception handling in backend for admin create/update/delete another admin's account

--- Backend/src/main/java/com/olivecrm/controller/EmployeeController.java
+++ Backend/src/main/java/com/olivecrm/controller/EmployeeController.java
@@ -47,7 +47,9 @@ public class EmployeeController {
     @PutMapping("/update/{username}")
     public ResponseEntity<?> updateUser(@PathVariable String username, @RequestBody EmployeeDTO userDTO) {
         try {
-            Employee.Role userRole = mapRole(userDTO.getRole());
+            // Only map the role if it is provided, otherwise pass null
+            Employee.Role userRole = userDTO.getRole() != null ? mapRole(userDTO.getRole()) : null;
+        
             Employee updateUser = employeeService.updateUser(username, userDTO.getFirst_name(), userDTO.getLast_name(),
                     userDTO.getPassword(), userRole);
             return ResponseEntity.ok(updateUser);
