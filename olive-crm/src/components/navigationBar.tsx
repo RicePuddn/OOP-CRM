5c5997ed9c83fe10d8b848fe8a2716953a838c77
Eclipse020 <happycelestee22@gmail.com>
2024-10-23 17:10:37 +0800

    Edited the navigationBar for marketing

--- olive-crm/src/components/navigationBar.tsx
+++ olive-crm/src/components/navigationBar.tsx
@@ -73,7 +73,7 @@ export default function AppSidebar() {
   } else if (role === "MARKETING") {
     items.push({
       title: "Reports",
-      url: "#",
+      url: "/reports",
       icon: BarChart,
     });
   }
